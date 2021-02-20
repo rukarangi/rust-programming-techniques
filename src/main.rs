89256df4b5272a0ced1446257e461f63e5ba5dcb
Luke <luke@yorkbay.net.nz>
2021-02-20 23:17:39 +1300

    first result example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn hio(i: i32) -> Result<i32, String> {
+    match i {
+        i if i >= 0 => Ok(i + 10),
+        _ => Err(format!("Input to h less than 0, found: {}", i))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // ---- result example -----
+    let input: i32 = 2;
+    match hio(input) {
+        Ok(i) => println!("Result: {}", i),
+        Err(e) => println!("Error: {}", e)
+    }
+    // let i = hio(input)?; - toit syntax
+    // can change to catch errors on lots of functions
+    // println!("{}", i);
+    // ----
 }
